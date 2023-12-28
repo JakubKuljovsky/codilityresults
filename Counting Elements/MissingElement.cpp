6059c887ef564860006ff8f86edba9ef50a11496
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2023-12-28 01:25:46 +0100

    Update MissingElement.cpp

--- Counting Elements/MissingElement.cpp
+++ Counting Elements/MissingElement.cpp
@@ -19,3 +19,20 @@ Write an efficient algorithm for the following assumptions:
  * N is an integer within the range [1..100,000];
  * each element of array A is an integer within the range [−1,000,000..1,000,000].
 */
+
+int solution(vector<int> &A) {
+    std::vector<bool> bVec(A.size() + 1, false);
+    for (unsigned i = 0; i < A.size(); ++i)
+    {
+        if (A[i] > 0 and A[i] <= A.size())
+        {
+            bVec[A[i]] = true;
+        }
+    }
+    if (bVec.empty()) return 1;
+    for (unsigned i = 1; i <= A.size(); ++i)
+    {
+        if (bVec[i] == false) return i;
+    }
+    return bVec.size();
+}
