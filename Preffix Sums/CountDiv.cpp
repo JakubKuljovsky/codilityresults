03ea8f147b9455d4c085cd72166d64f39693ed76
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2023-12-28 19:40:37 +0100

    Update CountDiv.cpp

--- Preffix Sums/CountDiv.cpp
+++ Preffix Sums/CountDiv.cpp
@@ -1,1 +1,14 @@
 
+int solution(int A, int B, int K) {
+    int number{0};
+    if (A == 0)
+    {
+        number = B / K;
+        number++;
+    }
+    else
+    {
+       number = (B / K) - ((A - 1) / K);
+    }
+    return number;
+}
