c500810d9766c541b133a61aa79808a3f198f91a
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-01-29 19:14:50 +0100

    Update NumberOfDiscIntersections.cpp

--- Sorting/NumberOfDiscIntersections.cpp
+++ Sorting/NumberOfDiscIntersections.cpp
@@ -21,3 +21,29 @@ Write an efficient algorithm for the following assumptions:
  * N is an integer within the range [0..100,000];
  * each element of array A is an integer within the range [0..2,147,483,647].
 */
+#include <algorithm>
+
+int solution(std::vector<int>& A) {
+    int totalIntersec{ 0 };
+    int size = static_cast<int>(A.size());
+    std::vector<int> Begin(size, 0);
+    std::vector<int> End(size, 0);
+
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        Begin[std::max(0, static_cast<int>(i - A[i]))]++;
+        End[std::min(static_cast<size_t>(i + A[i]), static_cast<size_t>(size - 1))]++;
+    }
+
+    int active{ 0 };
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        totalIntersec += active * Begin[i] + (Begin[i] * (Begin[i] - 1)) / 2;
+        if (totalIntersec > 10'000'000)
+        {
+            return -1;
+        }
+        active += Begin[i] - End[i];
+    }
+    return totalIntersec;
+}
