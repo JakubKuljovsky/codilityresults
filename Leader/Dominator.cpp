85d8ee97c969a72d7e2975dd9edbd2d44b46ddd7
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-02 14:40:48 +0100

    Update Dominator.cpp

--- Leader/Dominator.cpp
+++ Leader/Dominator.cpp
@@ -1,1 +1,23 @@
 
+#include <map>
+int solution(vector<int> &A) {
+    if (A.size() == 1)
+    {
+        return 0;
+    }
+    std::map<int, int> B;
+    int moereThanHalf{(static_cast<int>(A.size()) / 2) + 1};
+
+    for (unsigned int i = 0; i < A.size(); ++i)
+    {
+        if(!B.try_emplace(A[i], 1).second)
+        {
+            B[A[i]]++;
+            if (B[A[i]] == moereThanHalf)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
