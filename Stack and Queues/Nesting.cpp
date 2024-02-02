80da683b14cb4a82b2e4ce6f494f96299025df3e
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-02 13:22:06 +0100

    Update Nesting.cpp

--- Stack and Queues/Nesting.cpp
+++ Stack and Queues/Nesting.cpp
@@ -1,1 +1,32 @@
 
+#include <stack>
+int solution(string &S) {
+    if (S.empty())
+    {
+        return 1;
+    }
+    else if (S.size() % 2 != 0)
+    {
+        return 0;
+    }
+    std::stack<char> st;
+    for (char c: S)
+    {
+        switch(c)
+        {
+            case '(':
+                st.push(c);
+                break;
+            case ')':
+                if (st.top() != '(')
+                {
+                    return 0;
+                }
+                st.pop();
+                break;
+            default:
+               return 0;
+        }
+    }
+    return st.empty();
+}
