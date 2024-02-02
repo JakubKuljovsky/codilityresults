c8325995a2a8a12a1354de4aca905b7eb64e3406
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-02 14:16:45 +0100

    Update StoneWall.cpp

--- Stack and Queues/StoneWall.cpp
+++ Stack and Queues/StoneWall.cpp
@@ -1,1 +1,32 @@
 
+#include <stack>
+int solution(std::vector<int>& H) {
+    std::stack<int> st;
+    int numberOfBlock{ 0 };
+    for (unsigned int i = 0; i < H.size(); ++i)
+    {
+        while (st.size() > 0)
+        {
+            if (st.top() > H[i])
+            {
+                st.pop();
+                numberOfBlock++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (st.empty())
+        {
+            st.push(H[i]);
+            continue;
+        }
+        if (st.top() < H[i])
+        {
+            st.push(H[i]);
+        }
+    }
+    numberOfBlock += st.size();
+    return numberOfBlock;
+}
