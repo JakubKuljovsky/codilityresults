60492ad2fb10be63d041aa78215f1b73cf6f18b6
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-07 21:34:28 +0100

    Update Flags.cpp

--- Prime and composite numbers/Flags.cpp
+++ Prime and composite numbers/Flags.cpp
@@ -1,1 +1,34 @@
 
+
+int solution(vector<int> &A) {
+    if (A.size() < 3)
+    {
+        return 0;
+    }
+
+    int numberOfFlags{1};
+    int numberOfSetFlags{0};
+    int maxNumberOfFlags{0};
+
+    for (unsigned int i = 1; i < A.size() - 1; ++i)
+    {
+        if (A[i -1] < A[i] and A[i] > A[i + 1])
+        {
+            numberOfSetFlags++;
+            if (numberOfSetFlags == numberOfFlags)
+            {
+                maxNumberOfFlags = std::max(maxNumberOfFlags, numberOfFlags);
+                numberOfSetFlags = 0;
+                i = 0;
+                numberOfFlags++;
+
+            }
+            else
+            {
+                i += numberOfFlags - 1;
+            }
+        } 
+    }
+
+    return  maxNumberOfFlags;
+}
