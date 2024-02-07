db1afdee636df9bcd090aa887886699ff4377f00
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-07 20:49:22 +0100

    Update MinPerimeterRectangle.cpp

--- Prime and composite numbers/MinPerimeterRectangle.cpp
+++ Prime and composite numbers/MinPerimeterRectangle.cpp
@@ -1,1 +1,17 @@
 
+#include <cmath>
+
+int solution(int N) {
+
+   int sN = sqrt(N) + 1;
+
+   for (int i = sN; i > 1 ; --i)
+   {
+       if (N % i == 0)
+       {
+            return  2* (i + (N/i));
+       }
+   }
+
+   return 2 * (N + 1);
+}
