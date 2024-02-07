edb5281ccecd3f007ec2097c535f321741acd21f
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-07 20:34:54 +0100

    Update CountFactors.cpp

--- Prime and composite numbers/CountFactors.cpp
+++ Prime and composite numbers/CountFactors.cpp
@@ -1,1 +1,23 @@
 
+#include <cmath>
+
+int solution(int N) {
+
+   int numberOfFactor{0};
+   int sN = sqrt(N) + 1;
+
+   for (int i = 1; i < sN; ++i)
+   {
+       if (N % i == 0)
+       {
+            numberOfFactor++;
+
+            if (N / i > i)
+            {
+                numberOfFactor++;
+            }
+       }
+   }
+
+   return numberOfFactor;
+}
