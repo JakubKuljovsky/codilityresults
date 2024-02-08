e8c650303134071890c0e10f6f56cf76b580205e
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-02-08 19:15:38 +0100

    Update TieRopes.cpp

--- Greedy algorithms/TieRopes.cpp
+++ Greedy algorithms/TieRopes.cpp
@@ -1,1 +1,17 @@
 
+
+int solution(int K, vector<int> &A) {
+   int count{0};
+   int ropeLength{0};
+
+   for (unsigned int i = 0; i < A.size(); ++i)
+   {
+       ropeLength += A[i];
+       if (ropeLength >= K)
+       {
+            ropeLength = 0;
+            count++;
+       }
+   }
+   return count;
+}
