cc9c455c35ee8b0aca967192f71c0465ac9306e0
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2024-10-04 16:43:51 +0200

    Update CountTriangles.cpp

--- Caterpillar method/CountTriangles.cpp
+++ Caterpillar method/CountTriangles.cpp
@@ -1,45 +1,31 @@
 /*
-MinAbsSumOfTwo: Find the minimal absolute value of a sum of two elements.
+CountTriangles: Count the number of triangles that can be built from a given set of edges.
 ************************************************************************************************************************************************************
-Let A be a non-empty array consisting of N integers.
-
-The abs sum of two for a pair of indices (P, Q) is the absolute value |A[P] + A[Q]|,
-for 0 ≤ P ≤ Q < N.
-
-For example, the following array A:
-
-  A[0] =  1
-  A[1] =  4
-  A[2] = -3
-has pairs of indices (0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2).
-The abs sum of two for the pair (0, 0) is A[0] + A[0] = |1 + 1| = 2.
-The abs sum of two for the pair (0, 1) is A[0] + A[1] = |1 + 4| = 5.
-The abs sum of two for the pair (0, 2) is A[0] + A[2] = |1 + (−3)| = 2.
-The abs sum of two for the pair (1, 1) is A[1] + A[1] = |4 + 4| = 8.
-The abs sum of two for the pair (1, 2) is A[1] + A[2] = |4 + (−3)| = 1.
-The abs sum of two for the pair (2, 2) is A[2] + A[2] = |(−3) + (−3)| = 6.
+An array A consisting of N integers is given. A triplet (P, Q, R)
+is triangular if it is possible to build a triangle with sides of lengths A[P],
+A[Q] and A[R]. In other words, triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N
+and:
+  *  A[P] + A[Q] > A[R],
+  *  A[Q] + A[R] > A[P],
+  *  A[R] + A[P] > A[Q].
+  
+For example, consider array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 12
+There are four triangular triplets that can be constructed from elements of this array, 
+namely (0, 2, 4), (0, 2, 5), (0, 4, 5), and (2, 4, 5).
 
 Write a function:
-    int solution(vector<int> &A);
+     int solution(vector<int> &A);
 
-that, given a non-empty array A consisting of N integers,
-returns the minimal abs sum of two for any pair of indices in this array.
+that, given an array A consisting of N integers, returns the number of triangular triplets in this array.
 
-For example, given the following array A:
-  A[0] =  1
-  A[1] =  4
-  A[2] = -3
-the function should return 1, as explained above.
-
-Given array A:
-  A[0] = -8
-  A[1] =  4
-  A[2] =  5
-  A[3] =-10
-  A[4] =  3
-the function should return |(−8) + 5| = 3.
+For example, given array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 12
+the function should return 4, as explained above.
 
 Write an efficient algorithm for the following assumptions:
-  *  N is an integer within the range [1..100,000];
-  *  each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+  *  N is an integer within the range [0..1,000];
+  *  each element of array A is an integer within the range [1..1,000,000,000].
 */
