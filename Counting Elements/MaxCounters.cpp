6a047a1ccf7b6687abe04d5aa10b72b0db831f40
Jakub Kuljovský <33068003+JakubKuljovsky@users.noreply.github.com>
2025-01-20 22:22:15 +0100

    Update MaxCounters.cpp

--- Counting Elements/MaxCounters.cpp
+++ Counting Elements/MaxCounters.cpp
@@ -3,9 +3,8 @@ MaxCounters: Calculate the values of counters after applying all alternating ope
 increase counter by 1; set value of all counters to current maximum.
 -------------------------------------------------------------------------------------------------------------------------------------
 You are given N counters, initially set to 0, and you have two possible operations on them:
-
-increase(X) − counter X is increased by 1,
-max counter − all counters are set to the maximum value of any counter.
+ * increase(X) − counter X is increased by 1,
+ * max counter − all counters are set to the maximum value of any counter.
 A non-empty array A of M integers is given. This array represents consecutive operations:
  * if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
  * if A[K] = N + 1 then operation K is max counter.
